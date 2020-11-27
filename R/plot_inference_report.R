b8c139670cdfac8a6d044f14f7d2e20fb8d7348c
salvatore <militesalvatore@gmail.com>
2020-11-28 00:33:26 +0100

    bugfix

--- R/plot_inference_report.R
+++ R/plot_inference_report.R
@@ -1,6 +1,6 @@
 plot_inference_report <-  function(x) {
   Z_post <- Rcongas:::is_MAP_Z(x)
-  CN_post <- Rcongas:::is_MAP_CN(x)
+  CN_post <- !Rcongas:::is_MAP_CN(x)
 
   if (Z_post & CN_post) {
     return(plot_inference_report_1(x))
@@ -111,25 +111,47 @@ plot_normalization_factors <- function(x)
 {
   best_model <- Rcongas:::get_best_model(x)
 
-  df = data.frame(
-    mu = best_model$parameters$norm_factor,
-    cell = names(best_model$parameters$norm_factor),
-    stringsAsFactors = FALSE
-  ) %>%
-    as_tibble() %>%
-    left_join(
-      Rcongas:::get_clusters(x),
-      by = 'cell'
+  if(is_gaussian(x)){
+
+    df = data.frame(
+      mu = best_model$parameters$norm_sd,
+      segs = colnames(best_model$parameters$cnv_probs),
+      stringsAsFactors = FALSE
     )
 
-  ggplot(
-    data = df,
-    aes(x = mu, fill = cluster)
-  ) + geom_histogram(bins = 60) +
-    ggtitle("Distribution of library size factors") +
-    xlab("Library size factor") +
-    CNAqc:::my_ggplot_theme() +
-    scale_fill_manual(values = Rcongas:::get_clusters_colors(df$cluster))
+    ggplot(
+      data = df,
+      aes(x = mu)
+    ) + geom_histogram(bins = 60, color = "black", fill = "royalblue2") +
+      ggtitle("Distribution of segments standard deviations") +
+      xlab("SD") +
+      CNAqc:::my_ggplot_theme()
+
+  } else{
+
+    df = data.frame(
+      mu = best_model$parameters$norm_factor,
+      cell = names(best_model$parameters$norm_factor),
+      stringsAsFactors = FALSE
+    ) %>%
+      as_tibble() %>%
+      left_join(
+        Rcongas:::get_clusters(x),
+        by = 'cell'
+      )
+
+    ggplot(
+      data = df,
+      aes(x = mu, fill = cluster)
+    ) + geom_histogram(bins = 60) +
+      ggtitle("Distribution of library size factors") +
+      xlab("Library size factor") +
+      CNAqc:::my_ggplot_theme() +
+      scale_fill_manual(values = Rcongas:::get_clusters_colors(df$cluster))
+
+  }
+
+
 }
 
 
--- R/test_status.R
+++ R/test_status.R
@@ -5,12 +5,16 @@ has_DE = function(x)
 
 is_MAP_Z <-  function(x){
   best_model <- get_best_model(x)
-  return(best_model$run_information$posteriors)
+  if(is.null(best_model$run_information_old))
+    return(best_model$run_information$posteriors)
+  else
+    return(best_model$run_information_old$posteriors)
 }
 
 is_MAP_CN <-  function(x){
   best_model <- get_best_model(x)
   return(best_model$run_information$MAP)
+
 }
 
 has_inference <-  function(x){
